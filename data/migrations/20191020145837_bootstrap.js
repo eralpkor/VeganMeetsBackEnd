220046f8d31faec30268480a81736d5cc24aa527
eralpkor <eralp.ny@gmail.com>
2019-10-25 10:29:09 -0400

    login route :fire:

--- data/migrations/20191020145837_bootstrap.js
+++ data/migrations/20191020145837_bootstrap.js
@@ -1,10 +1,31 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('users', users => {
-    users.increments();
-    users.string('email', 255).notNullable()
-    users.string('username', 128).notNullable().unique();
-    users.string('password', 128).notNullable();
+  return knex.schema.createTable('restaurants', tbl => {
+    tbl.increments();
+    tbl.string('name', 255).notNullable();
+
+    tbl.increments();
+    tbl.string('email', 255).notNullable()
+    tbl.string('username', 128).notNullable().unique();
+    tbl.string('password', 128).notNullable();
+  })
+  .createTable('vegan_favorites', tbl => {
+    tbl.increments();
+    tbl.string('name', 255).notNullable();
+    tbl.text('notes');
+    tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE') // delete if parent deleted
+        .onDelete('RESTRICT');
+    
+    tbl.integer('restaurant_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('')
   })
 };
 
